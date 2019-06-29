bfddf6eaa69202f2fe0e873b345f6f11c45e5882
Stuart Bonham <stuart_bonham@blueyonder.co.uk>
2019-06-29 11:50:04 +0100

    Use grid for gui layout

--- multifilter/Source/PluginEditor.cpp
+++ multifilter/Source/PluginEditor.cpp
@@ -17,7 +17,7 @@ MultifilterAudioProcessorEditor::MultifilterAudioProcessorEditor (MultifilterAud
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (800, 300);
+    setSize (500, 200);
 
 	cutoffFreqControl.setSliderStyle(Slider::RotaryHorizontalVerticalDrag);
 	cutoffFreqControl.setTextBoxStyle(Slider::NoTextBox, false, 90, 0);
@@ -48,6 +48,7 @@ MultifilterAudioProcessorEditor::MultifilterAudioProcessorEditor (MultifilterAud
 		  "ButterBPF2", "ButterSF2", "MMALPF2", "MMALPF2B", "LowShelf", "HighShelf", "NCQParaEQ",
 		  "CQParaEQ", "LWRLPF2", "LWRHPF2", "APF1", "APF2", "ResonA", "ResonB", "MatchLP2A",
 		  "MatchLP2B", "MatchBP2A", "MatchBF2B", "ImpInvLP1", "ImpInvLP2" }, 1);
+
 	filterTypeValue = std::make_unique<AudioProcessorValueTreeState::ComboBoxAttachment>(processor.pluginState, "filter_type", filterTypeControl);
 	filterTypeControl.setText("Filter Type");
 	
@@ -69,8 +70,20 @@ void MultifilterAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
-	cutoffFreqControl.setBoundsRelative(0.05f, 0.1f, 0.1f, 0.1f);
-	qFactorControl.setBoundsRelative(0.15f, 0.1f, 0.1f, 0.1f);
-	boostControl.setBoundsRelative(0.25f, 0.1f, 0.1f, 0.1f);
-	filterTypeControl.setBoundsRelative(0.35f, 0.1f, 0.25f, 0.05f);
+
+	Grid grid;
+
+	grid.templateRows = { Grid::TrackInfo(1_fr) };
+	grid.templateColumns = { Grid::TrackInfo(1_fr), Grid::TrackInfo(1_fr), Grid::TrackInfo(1_fr), Grid::TrackInfo(2_fr) };
+
+	grid.items = { GridItem(cutoffFreqControl), 
+		GridItem(qFactorControl), 
+		GridItem(boostControl), 
+		GridItem(filterTypeControl)
+		    .withHeight(40)
+		    .withWidth(150)
+		    .withAlignSelf(GridItem::AlignSelf::center)
+	        .withJustifySelf(GridItem::JustifySelf::center) };
+
+	grid.performLayout(getLocalBounds());
 }
